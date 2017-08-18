6396dceef84a408caecdac78800a706e4cbfae98
Franck-Emile Gamess <franck.gamess@gmail.com>
2017-08-18 12:05:01 +0200

    implement use of response array

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -3,9 +3,12 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\ContactRequest;
+use AppBundle\Service\ResponseArrayBuilder;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
@@ -18,9 +21,11 @@ class UserController extends Controller
      */
     public function sendInvitationAction(ContactRequest $contactRequest)
     {
-        dump($contactRequest);
+//        dump($contactRequest);
         $this->get('app.structural.data_mapper.contact_request')->save($contactRequest, true);
-        die;
+        $responseArray = $this->get(ResponseArrayBuilder::class)->build(ResponseArrayBuilder::RESPONSES_TYPES['contact_request_created']);
+
+        return new JsonResponse($this->get('serializer')->serialize($responseArray, 'json'));
     }
 
     /**
@@ -29,10 +34,18 @@ class UserController extends Controller
      * @Rest\Post("/accept-invitation")
      * @Rest\View
      */
-    public function acceptInvitationAction(ContactRequest $contactRequest)
+    public function acceptInvitationAction(Request $request)
     {
+        $contactRequestId = $request->request->getInt('contactRequestId');
         $contactRequestMapper = $this->get('app.structural.data_mapper.contact_request');
-        dump($contactRequest);
-        die;
+        $success = $contactRequestMapper->acceptInvitation($contactRequestId);
+
+        if ($success) {
+            $responseArray = $this->get(ResponseArrayBuilder::class)->build(ResponseArrayBuilder::RESPONSES_TYPES['contact_request_accepted']);
+        } else {
+            $responseArray = $this->get(ResponseArrayBuilder::class)->build(ResponseArrayBuilder::RESPONSES_TYPES['contact_request_not_found']);
+        }
+
+        return new JsonResponse($this->get('serializer')->serialize($responseArray, 'json'));
     }
 }
