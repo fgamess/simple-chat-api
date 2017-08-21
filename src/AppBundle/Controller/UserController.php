2f578268c3db6a26a027ba5abd2490f7d15137a1
Franck GAMESS <hamza.almartiniki@gmail.com>
2017-08-21 22:51:59 +0200

    update UserController

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -5,15 +5,19 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\ContactRequest;
 use AppBundle\Service\ResponseArrayBuilder;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
-class UserController extends Controller
+class UserController extends FOSRestController
 {
     /**
      * [sendInvitationAction description]
+     * 
      * @param  ContactRequest $contactRequest [description]
      * @Rest\Post("/send-invitation")
      * @Rest\View
@@ -21,11 +25,44 @@ class UserController extends Controller
      */
     public function sendInvitationAction(ContactRequest $contactRequest)
     {
-//        dump($contactRequest);
         $this->get('app.structural.data_mapper.contact_request')->save($contactRequest, true);
         $responseArray = $this->get(ResponseArrayBuilder::class)->build(ResponseArrayBuilder::RESPONSES_TYPES['contact_request_created']);
 
-        return new JsonResponse($this->get('serializer')->serialize($responseArray, 'json'));
+        return $this->view(
+            $responseArray,
+            Response::HTTP_CREATED, 
+            [
+                'Location' => $this->generateUrl('get_contact_request', ['id' => $contactRequest->getId(), UrlGeneratorInterface::ABSOLUTE_URL])
+            ])
+        ;
+        /**
+         * The code above is equivalent to creating a JsonResponse to which you pass the same parameters.
+         */
+        // return new JsonResponse(
+        //     $responseArray,
+        //     Response::HTTP_CREATED,
+        //     [
+        //         'Location' => $this->generateUrl('get_contact_request', ['id' => $contactRequest->getId(), UrlGeneratorInterface::ABSOLUTE_URL])
+        //     ],
+        //     false
+        // );
+    }
+
+    /**
+     * Get a ContactRequest resource
+     * 
+     * @param ContactRequest $contactRequest
+     * @return void
+     * @Rest\View
+     * @Rest\Get(
+     *     path = "/contact-requests/{id}",
+     *     requirements = {"id"="\d+"}
+     * )
+     */
+    public function getContactRequestAction(ContactRequest $contactRequest)
+    {
+        return $contactRequest;
+        // return new JsonResponse($this->get('serializer')->serialize($contactRequest, 'json'), Response::HTTP_OK, [], true);
     }
 
     /**
@@ -46,6 +83,6 @@ class UserController extends Controller
             $responseArray = $this->get(ResponseArrayBuilder::class)->build(ResponseArrayBuilder::RESPONSES_TYPES['contact_request_not_found']);
         }
 
-        return new JsonResponse($this->get('serializer')->serialize($responseArray, 'json'));
+        return new JsonResponse($responseArray);
     }
 }
